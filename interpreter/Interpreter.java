1b6e824e682037e9fbdf57c8f0d6d7d88f00aae6
John Roberts <jhroberts@linkedin.com>
2019-11-12 18:36:37 -0800

    Adding skeleton code with additional implementation information

--- interpreter/Interpreter.java
+++ interpreter/Interpreter.java
@@ -4,28 +4,28 @@ import java.io.*;
 
 public class Interpreter {
 
-	ByteCodeLoader bcl;
+  ByteCodeLoader byteCodeLoader;
 
-	public Interpreter(String codeFile) {
-		try {
-			CodeTable.init();
-			bcl = new ByteCodeLoader(codeFile);
-		} catch (IOException e) {
-			System.out.println("**** " + e);
-		}
-	}
+  public Interpreter(String codeFile) {
+    try {
+      CodeTable.init();
+      byteCodeLoader = new ByteCodeLoader(codeFile);
+    } catch (IOException e) {
+      System.out.println("**** " + e);
+    }
+  }
 
-	void run() {
-		Program program = bcl.loadCodes();
-		VirtualMachine vm = new VirtualMachine(program);
-		vm.executeProgram();
-	}
+  void run() {
+    Program program = byteCodeLoader.loadCodes();
+    VirtualMachine vm = new VirtualMachine(program);
+    vm.executeProgram();
+  }
 
-	public static void main(String args[]) {
-		if (args.length == 0) {
-			System.out.println("***Incorrect usage, try: java interpreter.Interpreter <file>");
-			System.exit(1);
-		}
-		(new Interpreter(args[0])).run();
-	}
+  public static void main(String args[]) {
+    if (args.length == 0) {
+      System.out.println("***Incorrect usage, try: java interpreter.Interpreter <file>");
+      System.exit(1);
+    }
+    (new Interpreter(args[0])).run();
+  }
 }
