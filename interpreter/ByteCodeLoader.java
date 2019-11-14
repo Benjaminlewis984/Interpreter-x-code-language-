bdc065e7b94668b6fc165a7a77e0d148ba372997
Benjamin Lewis <benjaminlewis984@gmail.com>
2019-11-14 14:38:03 -0800

    Removed load codes and added to the load codes methods

--- interpreter/ByteCodeLoader.java
+++ interpreter/ByteCodeLoader.java
@@ -1,17 +1,56 @@
 package interpreter;
 
+import interpreter.bytecode.ByteCode;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.lang.reflect.*;
 
 //Responsible for seperating based on spaces. Reads each .ine of the code file. (Constctor does that). Sends first part of seperation
 //Splits into an array of strings. Offset in bytecode vector
 public class ByteCodeLoader {
+
+  private BufferedReader source;
+  private String nextLine;
+  private boolean isPriorEndLine = true;
+  LinkedList<String> items = new LinkedList<String>();
+
+
   public ByteCodeLoader(String byteCodeFile) throws IOException{
+      try {
+        source = new BufferedReader( new FileReader( byteCodeFile ));
+        while ( isPriorEndLine ) {
+        nextLine = source.readLine();
+        if ( nextLine == null ){
+            isPriorEndLine = false;
+            source.close();
+            break;
+        }
+        items.add(nextLine);
+        }
+      } catch (IOException e){
+        throw new IOException();
+      }
 
   }
 
 
-  public Program loadCodes() {
+  public Program loadCodes(LinkedList<String> items){
     Program program = new Program();
+    String[] tokens = new String[3];
+    String tokenHolder;
+
+    try{
+      for (int i = 0; i < items.size(); i++){
+        tokens = items.get(i).split(" ");
+      }
+      ByteCode code = (ByteCode) Class.forName("interpreter.bytecode." + tokens[0]).newInstance();
+    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e){
+      System.out.println("Load Codes***");
+    }
+
 
     return null;
   }
--- interpreter/CodeTable.java
+++ interpreter/CodeTable.java
@@ -1,5 +1,8 @@
 package interpreter;
 
+import interpreter.bytecode.HaltCode;
+import interpreter.bytecode.WriteCode;
+
 import java.util.Hashtable;
 
 //holds logic and hashmap for string references to their objects.
@@ -8,12 +11,24 @@ public class CodeTable {
 
   public static void init () {
     codeMap.put("LIT", "interpreter.bytecode.LitCode" );
-
-
+    codeMap.put("HALT", "HaltCode");
+    codeMap.put("Return", "ReturnCode");
+    codeMap.put("Bop", "BopCode");
+    codeMap.put("Call", "CallCode");
+    codeMap.put("FalseBranch", "FalseBranchCode");
+    codeMap.put("Pop", "PopCode");
+    codeMap.put("Read", "ReadCode");
+    codeMap.put("Write", "WriteCode");
+    codeMap.put("Store", "StoreCode");
+    codeMap.put("Args", "ArgsCode");
+    codeMap.put("Label", "LabelCode");
+    codeMap.put("Goto", "GotoCode");
+    codeMap.put("Load", "LoadCode");
   }
 
 //  returns value from hashmap
   public static String get(String code) {
-    return "";
+
+    return codeMap.get(code);
   }
 }
